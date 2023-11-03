3ad6778cf0dc76a16c1dc977efecb0316e959c16
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2023-11-03 22:33:39 +0900

    Day15

--- rvbear/Week5/1_12918_other.java
+++ rvbear/Week5/1_12918_other.java
@@ -1,1 +1,14 @@
-dd
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        try {
+            int num = Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+                return false;
+        }
+        return answer;
+    }
+}
