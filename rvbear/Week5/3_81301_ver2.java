53bb5b734d0a25dded07e084ea83e70169f11688
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2023-11-04 20:41:44 +0900

    Day17

--- rvbear/Week5/3_81301_ver2.java
+++ rvbear/Week5/3_81301_ver2.java
@@ -1,36 +1,11 @@
 import java.util.*;
 
 class Solution {
-    HashMap<String, String> map = new HashMap<>() {{
-        put("zero", "0");
-        put("one", "1");
-        put("two", "2");
-        put("three", "3");
-        put("four", "4");
-        put("five", "5");
-        put("six", "6");
-        put("seven", "7");
-        put("eight", "8");
-        put("nine", "9"); 
-    }};
     public int solution(String s) {
-        String answer = "";
-        String temp = "";
-        for(int i = 0; i < s.length(); i++) {
-            if((s.charAt(i) - '0') < 10 && (s.charAt(i) - '0') >= 0) {
-                if(!temp.equals("")) {
-                    answer += map.get(temp);
-                }
-                answer += s.charAt(i);
-                temp = "";
-            } else {
-                temp += s.charAt(i);
-                if(map.containsKey(temp)) {
-                    answer += map.get(temp);
-                    temp = "";
-                }
-            }
+       String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-        return Integer.parseInt(answer);
+        return Integer.parseInt(s);
     }
 }
