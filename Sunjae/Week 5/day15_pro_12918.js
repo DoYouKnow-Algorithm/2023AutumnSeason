d5dde38632993d46764953e2850ae61d5317736e
kimsunjae <woal9844@gmail.com>
2023-11-08 01:48:45 +0900

    day15~17

---
+++ Sunjae/Week 5/day15_pro_12918.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  var answer = true;
+
+  // 조건 1: 길이가 4 또는 6이어야 함
+  if (s.length != 4 && s.length != 6) {
+    answer = false;
+  }
+
+  // 조건 2: 숫자로만 구성되어야 함
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i);
+
+    if (c < "0" || c > "9") {
+      answer = false;
+      break;
+    }
+  }
+
+  return answer;
+}
