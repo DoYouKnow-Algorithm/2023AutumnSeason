02898d6eb5cdd0efe7224a3a4340971320b40df9
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2023-11-17 05:16:07 +0900

    수정

--- ayuriK152/7주차/p17681.cpp
+++ ayuriK152/7주차/p17681.cpp
@@ -1,19 +1,19 @@
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer;
     for (int i = 0; i < n; i++) {
-        string temp = "";   // answer 리스트에 추가될 한 줄의 결과값
-        for (int j = n - 1; j >= 0; j--) {
-            int current = pow(2, j);        // 매 회 비교할 2의 제곱수
-            if ((arr1[i] & current) == current || (arr2[i] & current) == current)       // and연산 후 제곱수와 비교해서 참값이면 벽
+        string temp = "";       // answer 리스트에 매 회차 대입할 결과값
+        int current = 1 << n - 1;       // 2의 n - 1승 시프트연산
+        for (int j = 0; j < n; j++) {
+            if ((arr1[i] & current) == current || (arr2[i] & current) == current)       // and연산하고 현재 2의 제곱수랑 하나라도 같으면 벽
                 temp.append("#");
             else
                 temp.append(" ");
+            current = current >> 1;         // 시프트 연산으로 나누기2
         }
         answer.push_back(temp);
     }
