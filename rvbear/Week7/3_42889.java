bdfe43581401040309aea77f65d795d6389a023f
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2023-11-22 13:56:39 +0900

    Day23

--- rvbear/Week7/3_42889.java
+++ rvbear/Week7/3_42889.java
@@ -9,8 +9,8 @@ class Solution {
         }
         
         List<Stage> cal = new ArrayList<>();
-        for(int i = 1; i <= N; i++) {
-            double temp = (double)arr[i] / n;
+        for(int i = 1; i <= N; i++) {        
+            double temp = (double)arr[i] / n;        // 실패율 계산
             n -= arr[i];
             
             Stage s = new Stage(i, temp);
@@ -26,16 +26,16 @@ class Solution {
         return answer;
     }
     
-    class Stage implements Comparable<Stage> {
-        int index;
-        double fail;
+    class Stage implements Comparable<Stage> {        // 스테이지 클래스
+        int index;        // 스테이지 번호
+        double fail;      // 실패한 사람
         
         public Stage(int _index, double _fail) {
             index = _index; fail = _fail;
         }
         
         @Override
-        public int compareTo(Stage o) {
+        public int compareTo(Stage o) {                // 정렬 오버라이딩
             return fail < o.fail ? -1 : (fail > o.fail ? 1 : 0);
         }
     }
